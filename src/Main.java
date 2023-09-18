4bb51eee3fb798d0aa77c015497d92711906260b
HampusCastle <Hampuscastle@gmail.com>
2023-09-18 14:51:26 +0200

    Intial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+/* TODO
+          --> uppgift1;
+          - Github name; HampusCastle
+          - Email: Hampuscastle@gmail.com
+          - https://github.com/HampusCastle
+        */
+
+        
+    }
+}
